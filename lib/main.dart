25dadde0aa5ff1cf84d99d8fc159c017f0e65f8e
OmarAmeer96 <omar.ameer244@gmail.com>
2024-12-19 15:30:26 +0200

    Refactor app structure and update imports

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:kouider_app/core/di/dependency_injection.dart';
 import 'package:kouider_app/core/helpers/simple_bloc_observer.dart';
 import 'package:kouider_app/core/routing/app_router.dart';
-import 'package:kouider_app/moonglish_app.dart';
+import 'package:kouider_app/kouider_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kouider_app/core/routing/app_router.dart';
 
-import 'package:kouider_app/moonglish_app.dart';
+import 'package:kouider_app/kouider_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
