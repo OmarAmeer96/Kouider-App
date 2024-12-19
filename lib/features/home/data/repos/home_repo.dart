2e17dfe486ffbe95af5ace4a64ed2dc1999493a8
OmarAmeer96 <omar.ameer244@gmail.com>
2024-12-19 17:24:06 +0200

    Implemented the HomeRepo

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,21 @@
+import 'package:kouider_app/core/networking/api_error_handler.dart';
+import 'package:kouider_app/core/networking/api_result.dart';
+import 'package:kouider_app/core/networking/api_service.dart';
+import 'package:kouider_app/features/home/data/models/products.dart';
+
+class HomeRepo {
+  final ApiService _apiService;
+
+  HomeRepo(this._apiService);
+
+  Future<ApiResult<Products>> getStudents(
+    int id,
+  ) async {
+    try {
+      final response = await _apiService.getProducts();
+      return ApiResult.success(response);
+    } catch (error) {
+      return ApiResult.failure(ErrorHandler.handle(error));
+    }
+  }
+}
