af0e138ac8360148a69ab7032b0bb086d241aa77
OmarAmeer96 <omar.ameer244@gmail.com>
2024-12-20 14:48:05 +0200

    Resolved the gradle error preventing from building apk.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kouider_app/core/routing/app_router.dart';
-
 import 'package:kouider_app/kouider_app.dart';
 
 void main() {
